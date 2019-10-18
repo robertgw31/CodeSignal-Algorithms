c62cc8ccdea826434e8e8f887a7a312c20a0e6bb
Robert Wittnebert <robertw@senutech.com>
2019-10-18 16:31:11 -0400

    improved commenting

--- FindProfesson/FindProfession.scala
+++ FindProfesson/FindProfession.scala
@@ -1,16 +1,29 @@
 object FindProfession extends App {
 
   def findProfession(level: Int, pos: Int): String = {
-    var curProf = "Engineer"
+    var curProf = ProfessionEngineer
+
+    // convert to base-0
     var curLevel = level -1
     var curPos = pos
+
     while(curLevel > 0) {
+      // each iteration performs calculations relative to the 'virtual root'
+      // of a subtree defined as level - curLevel - 1, relative to the target position 'pos'
+
+      // the number of postions on the subtree level
       val numLevelPos = Math.pow(2,curLevel).toInt
+
+      // segmentation of the level's positions between the two branches stemming off of the virtual root
       val break = numLevelPos / 2
+
+      // Profession and position are recalculated when navigating down the right branch
+      // off the virtual root
       if (curPos > break) {
-        curProf = if(curProf == "Engineer") "Doctor" else "Engineer"
+        curProf = if(curProf == ProfessionEngineer) ProfessionDoctor else ProfessionEngineer
         curPos = curPos - break;
       }
+      // re-root to next level down
       curLevel = curLevel - 1
     }
     curProf
